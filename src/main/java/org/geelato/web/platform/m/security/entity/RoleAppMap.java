eabc55f3ce0cf417b03552bfbf96d37b693cc9a6
geelato <geelato@126.com>
2020-04-07 19:36:34 +0800

    add roleAppMap

---
+++ src/main/java/org/geelato/web/platform/m/security/entity/RoleAppMap.java
@@ -0,0 +1,62 @@
+package org.geelato.web.platform.m.security.entity;
+
+
+import org.geelato.core.meta.annotation.Col;
+import org.geelato.core.meta.annotation.Entity;
+import org.geelato.core.meta.annotation.ForeignKey;
+import org.geelato.core.meta.annotation.Title;
+import org.geelato.core.meta.model.entity.BaseEntity;
+import org.geelato.web.platform.m.base.entity.App;
+
+@Entity(name = "platform_role_r_app")
+@Title(title = "角色APP关系表")
+public class RoleAppMap extends BaseEntity {
+    private Long roleId;
+
+    private Long appId;
+
+    private String appName;
+    private String roleName;
+
+    @Title(title = "角色ID")
+    @Col(name = "role_id", refTables = "platform_role", refColName = "platform_role.id")
+    @ForeignKey(fTable = Role.class)
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    @Title(title = "应用ID")
+    @Col(name = "app_id", refTables = "platform_app", refColName = "platform_app.id")
+    @ForeignKey(fTable = App.class)
+    public Long getAppId() {
+        return appId;
+    }
+
+    public void setAppId(Long appId) {
+        this.appId = appId;
+    }
+
+    @Title(title = "应用名称")
+    @Col(name = "app_name", isRefColumn = true, refLocalCol = "appId", refColName = "platform_app.name")
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    @Title(title = "角色名称")
+    @Col(name = "role_name", isRefColumn = true, refLocalCol = "roleId", refColName = "platform_role.name")
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
