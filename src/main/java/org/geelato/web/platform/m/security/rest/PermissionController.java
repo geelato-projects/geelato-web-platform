5880afe27c0b2852c1ef49a4a8c61e7af42c872f
diablo1303 <diabloposeidon@hotmail.com>
2023-05-18 10:21:56 +0800

    权限管理开发

---
+++ src/main/java/org/geelato/web/platform/m/security/rest/PermissionController.java
@@ -0,0 +1,128 @@
+package org.geelato.web.platform.m.security.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.util.Strings;
+import org.geelato.core.api.ApiPagedResult;
+import org.geelato.core.api.ApiResult;
+import org.geelato.web.platform.m.base.rest.BaseController;
+import org.geelato.web.platform.m.security.entity.DataItems;
+import org.geelato.web.platform.m.security.entity.ErrorMsg;
+import org.geelato.web.platform.m.security.entity.Permission;
+import org.geelato.web.platform.m.security.service.PermissionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author diabl
+ */
+@Controller
+@RequestMapping(value = "/api/security/permission")
+public class PermissionController extends BaseController {
+    private final Logger logger = LoggerFactory.getLogger(PermissionController.class);
+    @Autowired
+    private PermissionService permissionService;
+
+    @RequestMapping(value = "/pageQuery", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiPagedResult pageQuery(HttpServletRequest req) {
+        ApiPagedResult result = new ApiPagedResult();
+        try {
+            int pageNum = Strings.isNotBlank(req.getParameter("current")) ? Integer.parseInt(req.getParameter("current")) : -1;
+            int pageSize = Strings.isNotBlank(req.getParameter("pageSize")) ? Integer.parseInt(req.getParameter("pageSize")) : -1;
+            Map<String, Object> params = this.getQueryParameters(Permission.class, req);
+
+            List<Permission> pageQueryList = permissionService.pageQueryModel(Permission.class, pageNum, pageSize, params);
+            List<Permission> queryList = permissionService.queryModel(Permission.class, params);
+
+            result.setTotal(queryList != null ? queryList.size() : 0);
+            result.setData(new DataItems(pageQueryList, result.getTotal()));
+            result.setPage(pageNum);
+            result.setSize(pageSize);
+            result.setDataSize(pageQueryList != null ? pageQueryList.size() : 0);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult query(HttpServletRequest req) {
+        ApiResult result = new ApiResult();
+        try {
+            Map<String, Object> params = this.getQueryParameters(Permission.class, req);
+            return result.setData(permissionService.queryModel(Permission.class, params));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult get(@PathVariable(required = true) long id) {
+        ApiResult result = new ApiResult();
+        try {
+            return result.setData(permissionService.getModel(Permission.class, id));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/createOrUpdate", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResult createOrUpdate(@RequestBody Permission form) {
+        ApiResult result = new ApiResult();
+        try {
+            // ID为空方可插入
+            if (form.getId() != null && form.getId() > 0) {
+                // 存在，方可更新
+                if (permissionService.isExist(Permission.class, form.getId())) {
+                    result.setData(permissionService.updateModel(form));
+                } else {
+                    result.error().setMsg(ErrorMsg.IS_NULL);
+                }
+            } else {
+                result.setData(permissionService.createModel(form));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.OPERATE_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/isDelete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ApiResult isDelete(@PathVariable(required = true) long id) {
+        ApiResult result = new ApiResult();
+        try {
+            Permission mResult = permissionService.getModel(Permission.class, id);
+            if (mResult != null) {
+                permissionService.isDeleteModel(mResult);
+                result.success();
+            } else {
+                result.error().setMsg(ErrorMsg.IS_NULL);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.DELETE_FAIL);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/geelato/web/platform/m/security/service/PermissionService.java
@@ -0,0 +1,42 @@
+package org.geelato.web.platform.m.security.service;
+
+import org.geelato.web.platform.m.base.service.BaseService;
+import org.geelato.web.platform.m.security.entity.Permission;
+import org.geelato.web.platform.m.security.entity.RolePermissionMap;
+import org.geelato.web.platform.m.security.enums.DeleteStatusEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author diabl
+ */
+@Component
+public class PermissionService extends BaseService {
+    @Autowired
+    private RolePermissionMapService rolePermissionMapService;
+
+    /**
+     * 逻辑删除
+     *
+     * @param model
+     */
+    public void isDeleteModel(Permission model) {
+        // 用户删除
+        model.setDelStatus(DeleteStatusEnum.IS.getCode());
+        dao.save(model);
+        // 角色权限关系表
+        Map<String, Object> params = new HashMap<>();
+        params.put("permissionId", model.getId());
+        List<RolePermissionMap> rList = rolePermissionMapService.queryModel(RolePermissionMap.class, params);
+        if (rList != null) {
+            for (RolePermissionMap oModel : rList) {
+                oModel.setDelStatus(DeleteStatusEnum.IS.getCode());
+                dao.save(oModel);
+            }
+        }
+    }
+}
