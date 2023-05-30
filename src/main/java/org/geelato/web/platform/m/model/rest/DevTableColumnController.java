96fcb23554ad65585367ac59c4205cc6706d8b6f
diablo1303 <diabloposeidon@hotmail.com>
2023-05-30 09:07:03 +0800

    模型管理功能开发

---
+++ src/main/java/org/geelato/web/platform/m/model/rest/DevTableColumnController.java
@@ -0,0 +1,128 @@
+package org.geelato.web.platform.m.model.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.util.Strings;
+import org.geelato.core.api.ApiPagedResult;
+import org.geelato.core.api.ApiResult;
+import org.geelato.core.meta.model.field.ColumnMeta;
+import org.geelato.web.platform.m.base.rest.BaseController;
+import org.geelato.web.platform.m.model.service.DevTableColumnService;
+import org.geelato.web.platform.m.security.entity.DataItems;
+import org.geelato.web.platform.m.security.entity.ErrorMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author diabl
+ */
+@Controller
+@RequestMapping(value = "/api/model/table/column")
+public class DevTableColumnController extends BaseController {
+    private final Logger logger = LoggerFactory.getLogger(DevTableColumnController.class);
+    @Autowired
+    private DevTableColumnService devTableColumnService;
+
+    @RequestMapping(value = "/pageQuery", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiPagedResult<DataItems> pageQuery(HttpServletRequest req) {
+        ApiPagedResult<DataItems> result = new ApiPagedResult<>();
+        try {
+            int pageNum = Strings.isNotBlank(req.getParameter("current")) ? Integer.parseInt(req.getParameter("current")) : -1;
+            int pageSize = Strings.isNotBlank(req.getParameter("pageSize")) ? Integer.parseInt(req.getParameter("pageSize")) : -1;
+            Map<String, Object> params = this.getQueryParameters(ColumnMeta.class, req);
+
+            List<ColumnMeta> pageQueryList = devTableColumnService.pageQueryModel(ColumnMeta.class, pageNum, pageSize, params);
+            List<ColumnMeta> queryList = devTableColumnService.queryModel(ColumnMeta.class, params);
+
+            result.setTotal(queryList != null ? queryList.size() : 0);
+            result.setData(new DataItems(pageQueryList, result.getTotal()));
+            result.setPage(pageNum);
+            result.setSize(pageSize);
+            result.setDataSize(pageQueryList != null ? pageQueryList.size() : 0);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult<List<ColumnMeta>> query(HttpServletRequest req) {
+        ApiResult<List<ColumnMeta>> result = new ApiResult<>();
+        try {
+            Map<String, Object> params = this.getQueryParameters(ColumnMeta.class, req);
+            return result.setData(devTableColumnService.queryModel(ColumnMeta.class, params));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult<ColumnMeta> get(@PathVariable(required = true) long id) {
+        ApiResult<ColumnMeta> result = new ApiResult<>();
+        try {
+            return result.setData(devTableColumnService.getModel(ColumnMeta.class, id));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/createOrUpdate", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResult<Map> createOrUpdate(@RequestBody ColumnMeta form) {
+        ApiResult<Map> result = new ApiResult<>();
+        try {
+            // ID为空方可插入
+            if (form.getId() != null && form.getId() > 0) {
+                // 存在，方可更新
+                if (devTableColumnService.isExist(ColumnMeta.class, form.getId())) {
+                    result.setData(devTableColumnService.updateModel(form));
+                } else {
+                    result.error().setMsg(ErrorMsg.IS_NULL);
+                }
+            } else {
+                result.setData(devTableColumnService.createModel(form));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.OPERATE_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/isDelete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ApiResult isDelete(@PathVariable(required = true) long id) {
+        ApiResult result = new ApiResult();
+        try {
+            ColumnMeta mResult = devTableColumnService.getModel(ColumnMeta.class, id);
+            if (mResult != null) {
+                devTableColumnService.isDeleteModel(mResult);
+                result.success();
+            } else {
+                result.error().setMsg(ErrorMsg.IS_NULL);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.DELETE_FAIL);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/geelato/web/platform/m/model/rest/DevTableForeignController.java
@@ -0,0 +1,128 @@
+package org.geelato.web.platform.m.model.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.util.Strings;
+import org.geelato.core.api.ApiPagedResult;
+import org.geelato.core.api.ApiResult;
+import org.geelato.core.meta.model.entity.TableForeign;
+import org.geelato.web.platform.m.base.rest.BaseController;
+import org.geelato.web.platform.m.model.service.DevTableForeignService;
+import org.geelato.web.platform.m.security.entity.DataItems;
+import org.geelato.web.platform.m.security.entity.ErrorMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author diabl
+ */
+@Controller
+@RequestMapping(value = "/api/model/table/foreign")
+public class DevTableForeignController extends BaseController {
+    private final Logger logger = LoggerFactory.getLogger(DevTableForeignController.class);
+    @Autowired
+    private DevTableForeignService devTableForeignService;
+
+    @RequestMapping(value = "/pageQuery", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiPagedResult<DataItems> pageQuery(HttpServletRequest req) {
+        ApiPagedResult<DataItems> result = new ApiPagedResult<>();
+        try {
+            int pageNum = Strings.isNotBlank(req.getParameter("current")) ? Integer.parseInt(req.getParameter("current")) : -1;
+            int pageSize = Strings.isNotBlank(req.getParameter("pageSize")) ? Integer.parseInt(req.getParameter("pageSize")) : -1;
+            Map<String, Object> params = this.getQueryParameters(TableForeign.class, req);
+
+            List<TableForeign> pageQueryList = devTableForeignService.pageQueryModel(TableForeign.class, pageNum, pageSize, params);
+            List<TableForeign> queryList = devTableForeignService.queryModel(TableForeign.class, params);
+
+            result.setTotal(queryList != null ? queryList.size() : 0);
+            result.setData(new DataItems(pageQueryList, result.getTotal()));
+            result.setPage(pageNum);
+            result.setSize(pageSize);
+            result.setDataSize(pageQueryList != null ? pageQueryList.size() : 0);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult<List<TableForeign>> query(HttpServletRequest req) {
+        ApiResult<List<TableForeign>> result = new ApiResult<>();
+        try {
+            Map<String, Object> params = this.getQueryParameters(TableForeign.class, req);
+            return result.setData(devTableForeignService.queryModel(TableForeign.class, params));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult<TableForeign> get(@PathVariable(required = true) long id) {
+        ApiResult<TableForeign> result = new ApiResult<>();
+        try {
+            return result.setData(devTableForeignService.getModel(TableForeign.class, id));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/createOrUpdate", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResult<Map> createOrUpdate(@RequestBody TableForeign form) {
+        ApiResult<Map> result = new ApiResult<>();
+        try {
+            // ID为空方可插入
+            if (form.getId() != null && form.getId() > 0) {
+                // 存在，方可更新
+                if (devTableForeignService.isExist(TableForeign.class, form.getId())) {
+                    result.setData(devTableForeignService.updateModel(form));
+                } else {
+                    result.error().setMsg(ErrorMsg.IS_NULL);
+                }
+            } else {
+                result.setData(devTableForeignService.createModel(form));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.OPERATE_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/isDelete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ApiResult isDelete(@PathVariable(required = true) long id) {
+        ApiResult result = new ApiResult();
+        try {
+            TableForeign mResult = devTableForeignService.getModel(TableForeign.class, id);
+            if (mResult != null) {
+                devTableForeignService.isDeleteModel(mResult);
+                result.success();
+            } else {
+                result.error().setMsg(ErrorMsg.IS_NULL);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ErrorMsg.DELETE_FAIL);
+        }
+
+        return result;
+    }
+}
