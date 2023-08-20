6459d707af548357a2397b535518b9450bfdaff4
geelato <geelato@126.com>
2023-08-20 11:47:43 +0800

    Update RoleTreeNodeMap.java

--- src/main/java/org/geelato/web/platform/m/security/entity/RoleTreeNodeMap.java
+++ src/main/java/org/geelato/web/platform/m/security/entity/RoleTreeNodeMap.java
@@ -15,6 +15,8 @@ import org.geelato.web.platform.m.base.entity.TreeNode;
 @Entity(name = "platform_role_r_tree_node")
 @Title(title = "角色菜单关系表")
 public class RoleTreeNodeMap extends BaseEntity {
+
+    private String treeId;
     private String roleId;
 
     private String treeNodeId;
@@ -23,6 +25,16 @@ public class RoleTreeNodeMap extends BaseEntity {
     private String roleName;
 
     private String title;
+
+    @Title(title = "树ID")
+    @Col(name = "tree_id", nullable = false)
+    public String getTreeId() {
+        return treeId;
+    }
+
+    public void setTreeId(String treeId) {
+        this.treeId = treeId;
+    }
 
     @Title(title = "名称")
     @Col(name = "title", nullable = false)
