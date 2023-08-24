25bc552f7c67acc900786cb7c6b8c88a852402c2
diablo1303 <diabloposeidon@hotmail.com>
2023-08-24 10:50:47 +0800

    导出文件模板管理

---
+++ src/main/java/org/geelato/web/platform/m/excel/rest/ExportTemplateController.java
@@ -0,0 +1,130 @@
+package org.geelato.web.platform.m.excel.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.util.Strings;
+import org.geelato.core.api.ApiPagedResult;
+import org.geelato.core.api.ApiResult;
+import org.geelato.core.constants.ApiErrorMsg;
+import org.geelato.web.platform.m.base.rest.BaseController;
+import org.geelato.web.platform.m.excel.entity.ExportTemplate;
+import org.geelato.web.platform.m.excel.service.ExportTemplateService;
+import org.geelato.web.platform.m.security.entity.DataItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author diabl
+ * @description: TODO
+ * @date 2023/8/11 15:05
+ */
+@Controller
+@RequestMapping(value = "/api/export/template")
+public class ExportTemplateController extends BaseController {
+    private final Logger logger = LoggerFactory.getLogger(ExportTemplateController.class);
+    @Autowired
+    private ExportTemplateService exportTemplateService;
+
+    @RequestMapping(value = "/pageQuery", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiPagedResult pageQuery(HttpServletRequest req) {
+        ApiPagedResult result = new ApiPagedResult();
+        try {
+            int pageNum = Strings.isNotBlank(req.getParameter("current")) ? Integer.parseInt(req.getParameter("current")) : -1;
+            int pageSize = Strings.isNotBlank(req.getParameter("pageSize")) ? Integer.parseInt(req.getParameter("pageSize")) : -1;
+            Map<String, Object> params = this.getQueryParameters(ExportTemplate.class, req);
+
+            List<ExportTemplate> pageQueryList = exportTemplateService.pageQueryModel(ExportTemplate.class, pageNum, pageSize, params);
+            List<ExportTemplate> queryList = exportTemplateService.queryModel(ExportTemplate.class, params);
+
+            result.setTotal(queryList != null ? queryList.size() : 0);
+            result.setData(new DataItems(pageQueryList, result.getTotal()));
+            result.setPage(pageNum);
+            result.setSize(pageSize);
+            result.setDataSize(pageQueryList != null ? pageQueryList.size() : 0);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ApiErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult query(HttpServletRequest req) {
+        ApiResult result = new ApiResult();
+        try {
+            Map<String, Object> params = this.getQueryParameters(ExportTemplate.class, req);
+            return result.setData(exportTemplateService.queryModel(ExportTemplate.class, params));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ApiErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ApiResult get(@PathVariable(required = true) String id) {
+        ApiResult result = new ApiResult();
+        try {
+            return result.setData(exportTemplateService.getModel(ExportTemplate.class, id));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ApiErrorMsg.QUERY_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/createOrUpdate", method = RequestMethod.POST)
+    @ResponseBody
+    public ApiResult createOrUpdate(@RequestBody ExportTemplate form) {
+        ApiResult result = new ApiResult();
+        try {
+            // ID为空方可插入
+            if (Strings.isNotBlank(form.getId())) {
+                // 存在，方可更新
+                if (exportTemplateService.isExist(ExportTemplate.class, form.getId())) {
+                    result.setData(exportTemplateService.updateModel(form));
+                } else {
+                    result.error().setMsg(ApiErrorMsg.IS_NULL);
+                }
+            } else {
+                result.setData(exportTemplateService.createModel(form));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ApiErrorMsg.OPERATE_FAIL);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/isDelete/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ApiResult isDelete(@PathVariable(required = true) String id) {
+        ApiResult result = new ApiResult();
+        try {
+            ExportTemplate mResult = exportTemplateService.getModel(ExportTemplate.class, id);
+            if (mResult != null) {
+                exportTemplateService.isDeleteModel(mResult);
+                result.success();
+            } else {
+                result.error().setMsg(ApiErrorMsg.IS_NULL);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            result.error().setMsg(ApiErrorMsg.DELETE_FAIL);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/geelato/web/platform/m/excel/service/ExportTemplateService.java
@@ -0,0 +1,14 @@
+package org.geelato.web.platform.m.excel.service;
+
+import org.geelato.web.platform.m.base.service.BaseService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author diabl
+ * @description: TODO
+ * @date 2023/8/11 15:05
+ */
+@Component
+public class ExportTemplateService extends BaseService {
+
+}
