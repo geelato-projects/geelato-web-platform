fb6b2af92611b93909047b0d5cdb71299fa05db2
chengx <chengx@bingosoft.net>
2024-01-28 15:16:21 +0800

    ToMapperDBObject，用于映射数据库结构

--- src/main/java/org/geelato/web/platform/boot/BaseConfiguration.java
+++ src/main/java/org/geelato/web/platform/boot/BaseConfiguration.java
@@ -1,5 +1,6 @@
 package org.geelato.web.platform.boot;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -12,7 +13,7 @@ public class BaseConfiguration implements ApplicationContextAware {
 
     protected ApplicationContext applicationContext;
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@NotNull ApplicationContext applicationContext) throws BeansException {
        this.applicationContext = applicationContext;
     }
 
