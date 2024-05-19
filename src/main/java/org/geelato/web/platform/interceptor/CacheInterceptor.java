08e416e9d9562b080b95d307796ff070ab8fc524
geelato <geelato@126.com>
2024-05-20 00:18:19 +0800

    暂停cache

--- src/main/java/org/geelato/web/platform/interceptor/CacheInterceptor.java
+++ src/main/java/org/geelato/web/platform/interceptor/CacheInterceptor.java
@@ -21,29 +21,29 @@ public class CacheInterceptor implements HandlerInterceptor {
     CacheService cacheService=new CacheServiceImpl<>();
     @Override
     public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
-        boolean cacheOption= Boolean.parseBoolean(request.getHeader("cache"));
-        if(cacheOption){
-            String gql=getGql(request);
-            if(cacheService.getCache(gql)!=null) {
-                response.getWriter().write(cacheService.getCache(gql).toString());
-                return false;
-            }
-        }
+//        boolean cacheOption= Boolean.parseBoolean(request.getHeader("cache"));
+//        if(cacheOption){
+//            String gql=getGql(request);
+//            if(cacheService.getCache(gql)!=null) {
+//                response.getWriter().write(cacheService.getCache(gql).toString());
+//                return false;
+//            }
+//        }
         return true;
     }
 
     @Override
     public void postHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler, ModelAndView modelAndView) throws Exception {
-        CustomHttpServletResponse customHttpServletResponse=(CustomHttpServletResponse)response;
-        boolean cacheOption= Boolean.parseBoolean(request.getHeader("cache"));
-        if(cacheOption){
-            String gql=getGql(request);
-            if(cacheService.getCache(gql)==null) {
-                String data= customHttpServletResponse.getData();
-                cacheService.putCache(gql,data);
-                customHttpServletResponse.getWriter().write(data);
-            }
-        }
+//        CustomHttpServletResponse customHttpServletResponse=(CustomHttpServletResponse)response;
+//        boolean cacheOption= Boolean.parseBoolean(request.getHeader("cache"));
+//        if(cacheOption){
+//            String gql=getGql(request);
+//            if(cacheService.getCache(gql)==null) {
+//                String data= customHttpServletResponse.getData();
+//                cacheService.putCache(gql,data);
+//                customHttpServletResponse.getWriter().write(data);
+//            }
+//        }
     }
 
 
