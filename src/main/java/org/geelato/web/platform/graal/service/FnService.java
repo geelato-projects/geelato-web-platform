a97d0acf67a8c8f0df1a8e9edea10461bc246c87
chengx <chengx@bingosoft.net>
2024-06-17 09:10:47 +0800

    完善gql脚本

--- src/main/java/org/geelato/web/platform/graal/service/FnService.java
+++ src/main/java/org/geelato/web/platform/graal/service/FnService.java
@@ -2,8 +2,15 @@ package org.geelato.web.platform.graal.service;
 
 import com.ibm.icu.impl.duration.impl.Utils;
 import org.geelato.core.graal.GraalService;
+import org.geelato.core.util.StringUtils;
 import org.geelato.utils.NumbChineseUtils;
 import org.geelato.web.platform.m.security.service.UserService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @GraalService(name="fn",built = "true")
 public class FnService {
@@ -16,7 +23,17 @@ public class FnService {
         return NumbChineseUtils.byOldChinese(digit);
     }
 
-    public String dateText(){
-        return null;
+    public String dateText(String targetFormat,String dateStr) throws ParseException {
+        String formatDate=null;
+        Date date=null ;
+        SimpleDateFormat targetDateFormat=new SimpleDateFormat(targetFormat);
+        if(StringUtils.isEmpty(dateStr)){
+            date=new Date();
+            formatDate=targetDateFormat.format(date);
+        }else{
+            date = targetDateFormat.parse(dateStr);
+            formatDate=targetDateFormat.format(date);
+        }
+        return formatDate;
     }
 }
