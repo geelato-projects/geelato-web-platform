1a9870505bf622d19482705ec9b316fc42a0335d
chengx <chengx@bingosoft.net>
2024-06-26 14:28:35 +0800

    修改结构

--- src/main/java/org/geelato/web/platform/TestEventListener.java
+++ src/main/java/org/geelato/web/platform/TestEventListener.java
@@ -1,12 +1,13 @@
 package org.geelato.web.platform;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestEventListener implements ApplicationListener<MsgEvent> {
     @Override
-    public void onApplicationEvent(MsgEvent event) {
+    public void onApplicationEvent(@NotNull MsgEvent event) {
         System.out.println("receive msg event: " + event);
     }
 }
